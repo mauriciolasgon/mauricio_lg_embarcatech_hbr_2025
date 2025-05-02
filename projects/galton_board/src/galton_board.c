f4fa032629158f6c064b2106f7c9418466608637
mauricio <mauriciolasgon@gmail.com>
2025-05-02 16:49:11 -0300

    Criação da função de colisão e movimento horizontal. Modificação do nome da função move_bola para move_bola_y

--- projects/galton_board/src/galton_board.c
+++ projects/galton_board/src/galton_board.c
@@ -21,60 +21,114 @@ typedef struct {
 }bola;
 
 
+int binary_choice(); // Função que retorna um número binário aleatório observando o ruido do adc
+int colisao(bola *b, uint8_t *ssd); // Função que detecta a colisão da bolinha com os pinos
+void move_bola_x(bola *b,uint8_t *ssd, int direction); // Função que movimenta a bolinha horizontalmente
+void move_bola_y(bola *b, uint8_t *ssd, struct render_area *area); // Função que movimenta a bolinha no display de acordo com a velocidade
+void draw_canaleta(uint8_t *ssd); // Função que desenha as canaletas na posição central do display
+void draw_pins(uint8_t *ssd); // Função que desenha os pinos no display oled
 
+
+// Deteca a colisão da bolinha com os pinos sendo que a detecção é 1 
+// Se não houver colisão, retorna 0
+int colisao(bola *b, uint8_t *ssd)
+{
+    const int W = ssd1306_width;
+    if(ssd[b->posicao[0] + (b->posicao[2]/8) * W] & b->face<<b->v //Verifica se colisão na página atual
+        || ssd[b->posicao[1] + (b->posicao[2]/8) * W] & b->face<<b->v
+        || ssd[b->posicao[0] + ((b->posicao[2]+8)/8) * W] &  b->face>>(8-b->v) // Verifica se colisão na página seguinte
+        || ssd[b->posicao[1] + ((b->posicao[2]+8)/8) * W] &  b->face>>(8-b->v)){
+        
+            return 1; // colisão detectada
+    }
+    return 0; // sem colisão
+}
+
+//Movimenta a bolinha horizontalmente
+void move_bola_x(bola *b, uint8_t *ssd ,int direction)
+{
+    static int dir=1; // a bola comeca a esquerda do primeiro pino
+    int pos_l=0,pos_r=0;
+    uint8_t mask0 =ssd[b->posicao[0] + (b->posicao[2]/8) * ssd1306_width];
+    uint8_t mask1 =ssd[b->posicao[1] + (b->posicao[2]/8) * ssd1306_width];
+    ssd[b->posicao[0] + (b->posicao[2]/8) * ssd1306_width]=mask0 & ~ b->face; // Apaga apenas a bolinha na posição anterior
+    ssd[b->posicao[1] + (b->posicao[2]/8) * ssd1306_width]=mask1 & ~ b->face; 
+    if(dir== 1){// verifica se a bolinha colidiu pelo lado esquerdo e direito e adpta a nova posicao da bolinha
+        pos_l=5;
+        pos_r=4;
+        dir=0;
+    }else{
+        pos_l=4;
+        pos_r=5;
+        dir=1;
+    }
+    if(direction){// Se for esquerda
+        b->posicao[0]+=pos_l;
+        b->posicao[1]= b->posicao[0]+1;
+    }else{ // Se for direita
+        b->posicao[0]-=pos_r;
+        b->posicao[1]= b->posicao[0]-1;
+    } 
+}
 
 // Movimenta a bolinha no display de acordo com a velocidade
-void move_bola(bola *b, uint8_t *ssd, struct render_area *area)
+void move_bola_y(bola *b, uint8_t *ssd, struct render_area *area)
 {
-    static int posicao=0; 
-    // Desloca circularmente os bits  referentes a construção da bola de acordo com a velocidade
-    if(b->face>>(8-b->v) > 0){
-        ssd[b->posicao[0] + (b->posicao[2]/8) * ssd1306_width] |= b->face<<b->v;
-        ssd[b->posicao[1] + (b->posicao[2]/8) * ssd1306_width] |= b->face<<b->v;
-        b->face = b->face >> (8-b->v);
-        posicao=b->posicao[2]; // guarda a posição da ultima página 
-        b->posicao[2]+=8;
-        ssd[b->posicao[0] + (b->posicao[2]/8) * ssd1306_width] |= b->face;
-        ssd[b->posicao[1] + (b->posicao[2]/8) * ssd1306_width] |= b->face;
+    static int posicao=0;
+    
+    if(colisao(b, ssd)){ // Verifica se a bolinha colidiu com os pinos
+        
+        move_bola_x(b, ssd, binary_choice());   
+    }else{ 
+        // Desloca circularmente os bits  referentes a construção da bola de acordo com a velocidade
+        if(b->face>>(8-b->v) > 0){// Verifica se a bolinha mudou de página
+            ssd[b->posicao[0] + (b->posicao[2]/8) * ssd1306_width] = b->face<<b->v;
+            ssd[b->posicao[1] + (b->posicao[2]/8) * ssd1306_width] = b->face<<b->v;
+            b->face = b->face >> (8-b->v);
+            posicao=b->posicao[2]; // guarda a posição da ultima página 
+            b->posicao[2]+=8;
+            ssd[b->posicao[0] + (b->posicao[2]/8) * ssd1306_width] = b->face;
+            ssd[b->posicao[1] + (b->posicao[2]/8) * ssd1306_width] = b->face;
 
-    }else{
-        if(posicao!=0){ // Verifica se o pixel da bolinha já foi apagado na pagina anterior
-            ssd[b->posicao[0] + (posicao/8) * ssd1306_width] = 0x00;
-            ssd[b->posicao[1] + (posicao/8) * ssd1306_width] = 0x00;
-            posicao=0;
-        }
-        if(b->face==0x01){// ajusta a face da bolinha para continuar com 2 casas
-            b->face=b->face<<1;
-            b->face=b->face+1;
-            
-            
         }else{
-            b->face=b->face << b->v;
+            if(posicao!=0){ // Verifica se o pixel da bolinha já foi apagado na pagina anterior
+                ssd[b->posicao[0] + (posicao/8) * ssd1306_width] = 0x00;
+                ssd[b->posicao[1] + (posicao/8) * ssd1306_width] = 0x00;
+                posicao=0;
+            }
+            if(b->face==0x01){// ajusta a face da bolinha para continuar com 2 casas
+                b->face=b->face<<1;
+                b->face=b->face+1;
+                
+                
+            }else{
+                b->face=b->face << b->v; //Deslocamento da bolinha com base na velocidade
+            }
+        
+            ssd[b->posicao[0] + (b->posicao[2]/8) * ssd1306_width] = b->face;
+            ssd[b->posicao[1] + (b->posicao[2]/8) * ssd1306_width] = b->face;
         }
-       
-        ssd[b->posicao[0] + (b->posicao[2]/8) * ssd1306_width] |= b->face;
-        ssd[b->posicao[1] + (b->posicao[2]/8) * ssd1306_width] |= b->face;
     }
+
     
     // Verifica se a bola saiu do display
-    if (b->posicao[0] >= ssd1306_height) {
-        b->posicao[0] = 0;
-        b->posicao[1] = 1;
+    if (b->posicao[2] >= ssd1306_height) {
         b->posicao[2] = 0;
+
     }
     
  
     // Renderiza o display
     render_on_display(ssd, area);
 
-
 }
 
 // Função que retorna um número binário aleatório observando o ruido do adc
-
+//Retorna 1 para esquerda e 0 para direita
 int binary_choice(){
 
     uint16_t adc_value = adc_read();
+    printf("ADC Value: %d\n", (adc_value & 0x01));
     return (adc_value & 0x01); // Retorna o bit menos significativo do valor lido
 }
 
@@ -110,7 +164,7 @@ void draw_pins(uint8_t *ssd) {
         pin_count++;
         // espaçar cada “pino” em 2 colunas
         for (int j = 0; j < pin_count; j++) {
-            int idx = base + (j << 3);   // j*2 colunas
+            int idx = base + (j << 3 );   // j*2 colunas
             ssd[idx] |= mask;            // OR para não apagar outros pixels
         }
         start_x-=4;
@@ -160,11 +214,11 @@ int main()
     render_on_display(ssd, &frame_area);
     
     bola b1;
-    b1.v = 8;
-    b1.posicao[0] = ssd1306_width/2 -1;
+    b1.v = 2;
+    b1.posicao[0] = ssd1306_width/2  - 1;
     b1.posicao[1] = b1.posicao[0]+1;
     b1.posicao[2] = 0;
-    b1.face = 0x03; // 0xC0 é o caractere de preenchimento do display
+    b1.face = 0x03; //preenchimento dos dois primeiros bits de cada coluna
 
     ssd[b1.posicao[0] + (b1.posicao[2]/8) * ssd1306_width] = b1.face;
 
@@ -182,10 +236,7 @@ int main()
 
     while (true) {
         
-        
-        move_bola(&b1, ssd, &frame_area);
-        printf("Binary choice: %d\n", binary_choice());
-        
+        move_bola_y(&b1, ssd, &frame_area);  
         sleep_ms(1000);
     }
 }
