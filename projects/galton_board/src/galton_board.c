f66eb53a263f469ebd591413466bb3478b0e337f
mauricio <mauriciolasgon@gmail.com>
2025-05-02 11:12:58 -0300

    Ajuste das funções de desenhar pinos e canaletas

--- projects/galton_board/src/galton_board.c
+++ projects/galton_board/src/galton_board.c
@@ -20,27 +20,30 @@ typedef struct {
 
 }bola;
 
+
+
+
 // Movimenta a bolinha no display de acordo com a velocidade
 void move_bola(bola *b, uint8_t *ssd, struct render_area *area)
 {
     static int posicao=0; 
     // Desloca circularmente os bits  referentes a construção da bola de acordo com a velocidade
     if(b->face>>(8-b->v) > 0){
-        ssd[b->posicao[0] + (b->posicao[2]/8) * ssd1306_width] = b->face<<b->v;
-        ssd[b->posicao[1] + (b->posicao[2]/8) * ssd1306_width] = b->face<<b->v;
+        ssd[b->posicao[0] + (b->posicao[2]/8) * ssd1306_width] |= b->face<<b->v;
+        ssd[b->posicao[1] + (b->posicao[2]/8) * ssd1306_width] |= b->face<<b->v;
         b->face = b->face >> (8-b->v);
-        posicao=b->posicao[2];
+        posicao=b->posicao[2]; // guarda a posição da ultima página 
         b->posicao[2]+=8;
-        ssd[b->posicao[0] + (b->posicao[2]/8) * ssd1306_width] = b->face;
-        ssd[b->posicao[1] + (b->posicao[2]/8) * ssd1306_width] = b->face;
+        ssd[b->posicao[0] + (b->posicao[2]/8) * ssd1306_width] |= b->face;
+        ssd[b->posicao[1] + (b->posicao[2]/8) * ssd1306_width] |= b->face;
 
     }else{
-        if(posicao!=0){
+        if(posicao!=0){ // Verifica se o pixel da bolinha já foi apagado na pagina anterior
             ssd[b->posicao[0] + (posicao/8) * ssd1306_width] = 0x00;
             ssd[b->posicao[1] + (posicao/8) * ssd1306_width] = 0x00;
             posicao=0;
         }
-        if(b->face==0x01){
+        if(b->face==0x01){// ajusta a face da bolinha para continuar com 2 casas
             b->face=b->face<<1;
             b->face=b->face+1;
             
@@ -49,8 +52,8 @@ void move_bola(bola *b, uint8_t *ssd, struct render_area *area)
             b->face=b->face << b->v;
         }
        
-        ssd[b->posicao[0] + (b->posicao[2]/8) * ssd1306_width] = b->face;
-        ssd[b->posicao[1] + (b->posicao[2]/8) * ssd1306_width] = b->face;
+        ssd[b->posicao[0] + (b->posicao[2]/8) * ssd1306_width] |= b->face;
+        ssd[b->posicao[1] + (b->posicao[2]/8) * ssd1306_width] |= b->face;
     }
     
     // Verifica se a bola saiu do display
@@ -81,11 +84,11 @@ void draw_canaleta(uint8_t *ssd) {
     const int H = ssd1306_height;
     int y = H - 8;              // pixel vertical onde quer a “canaleta”
     int page = y >> 3;          // y/8
-    int bit  = y &  0x7;        // y%8
-    uint8_t mask = 1 << bit;    
+
+    uint8_t mask = 0xFF; // máscara para preencher a linha inteira
 
     int row_offset = page * W;
-    for (int x = W/4; x < W - W/4; x += 5) {
+    for (int x = W/4 +4 ; x < W - W/4; x += 8) {
         ssd[row_offset + x] |= mask;
     }
 }
@@ -95,20 +98,23 @@ void draw_canaleta(uint8_t *ssd) {
 void draw_pins(uint8_t *ssd) {
     const int W = ssd1306_width;
     const int H = ssd1306_height;
+    int pin_count = 0;
+    int start_x=W/2; 
+    for (int y = 8; y < H - 8; y+=8) {
 
-    for (int y = 1; y < H - 8; y++) {
-        int pin_count = y - 1;
-        int start_x   = W/2 - pin_count;
         int page      = y >> 3;      // qual página de 8 linhas
         int bit       = y & 0x7;     // qual bit dentro do byte
         uint8_t mask = 1 << bit;
         int base = page * W + start_x;
-
+        
+        pin_count++;
         // espaçar cada “pino” em 2 colunas
         for (int j = 0; j < pin_count; j++) {
-            int idx = base + (j << 1);   // j*2 colunas
+            int idx = base + (j << 3);   // j*2 colunas
             ssd[idx] |= mask;            // OR para não apagar outros pixels
         }
+        start_x-=4;
+       
     }
 }
 
@@ -155,9 +161,9 @@ int main()
     
     bola b1;
     b1.v = 8;
-    b1.posicao[0] = 5;
+    b1.posicao[0] = ssd1306_width/2 -1;
     b1.posicao[1] = b1.posicao[0]+1;
-    b1.posicao[2] = 8;
+    b1.posicao[2] = 0;
     b1.face = 0x03; // 0xC0 é o caractere de preenchimento do display
 
     ssd[b1.posicao[0] + (b1.posicao[2]/8) * ssd1306_width] = b1.face;
@@ -171,13 +177,13 @@ int main()
 
     render_on_display(ssd, &frame_area);
 
-    printf("Pins drawn\n");
+
 
 
     while (true) {
         
-        printf("Hello, world!\n");
-        //move_bola(&b1, ssd, &frame_area);
+        
+        move_bola(&b1, ssd, &frame_area);
         printf("Binary choice: %d\n", binary_choice());
         
         sleep_ms(1000);
